66ddae4b1a76c4dffa805d23276a4de09a45e465
Mikhail Svetov <svmk-tmsk@yandex.ru>
2018-10-08 15:32:45 +0700

    Implemented swagger-i18n-extension.

---
+++ README.md
@@ -0,0 +1,89 @@
+Swagger i18n extension
+----------------------
+
+Compiles swagger file into specified language:
+
+```
+npm install swagger-i18n-extension
+```
+
+To use this extension you need to specify vendor attribute `x-*-i18n` with translations object.
+Example:
+
+```
+openapi: 3.0.0
+info:
+  version: 0.0.2
+  title: Swagger example in default language
+  x-title-i18n:
+    eng: Title on english
+  description: This text will be translated in english
+  x-description-i18n:
+    eng: Description on english 
+```
+
+This swagger yaml will be translated into:
+```
+openapi: 3.0.0
+info:
+  version: 0.0.2
+  title: Title on english
+  description: Description on english
+```
+
+## Cli tool
+
+```
+$ swagger-i18n-extension
+Usage: swagger-i18n-extension <command> [options]
+
+Commands:
+  swagger-i18n-extension lang-list <path>         Print list of all languages described within swagger file
+  swagger-i18n-extension lang-list-all            Print list of all supported languages
+  swagger-i18n-extension translate <path> <lang>  Translates swagger file into specified language
+  swagger-i18n-extension translate-all <path>     Translates swagger file into all declared languages
+  swagger-i18n-extension validate <path>          Validates swagger i18n-extension.
+
+Options:
+  --version   Show version number                                      [boolean]
+  -h, --help  Show help                                                [boolean]
+
+Not enough non-option arguments: got 0, need at least 1
+
+```
+
+## Library documentation
+
+List of exported functions:
+### translate
+Translates swagger object into specified language.
+
+**Arguments**:
+| Argument | Type   | Description                                 |
+|----------|--------|---------------------------------------------|
+| object   | object | Swagger raw object after parse yaml or json |
+| language | string | Language ISO6392B code                      |
+
+**Return value**:
+Translated swagger `object`
+
+### getUsedLanguageCodes
+Returns list of used ISO6392B language codes
+
+**Arguments**:
+| Argument | Type   | Description                                 |
+|----------|--------|---------------------------------------------|
+| object   | object | Swagger raw object after parse yaml or json |
+
+**Return value**:
+`[]string` with used ISO6392B language code
+
+### validate
+| Argument   | Type   | Description                                 |
+|------------|--------|---------------------------------------------|
+| object     | object | Swagger raw object after parse yaml or json |
+| throwError | bool   | Throw error if true. Else - return value    |
+
+ **Return value**:
+ `True` if object is valid.
+ `False` if object is invalid.
---
+++ index.js
@@ -0,0 +1,99 @@
+var iterator = require('deep-for-each');
+var iso6392Codes = require('iso-639-2');
+var regexp = /^x-([a-zA-Z_-]+[a-zA-Z0-9_-]*)-i18n$/;
+
+function validateLanguageCode(language) {
+    if (typeof(language) === 'undefined') {
+        throw new Error('Language code was not passed')
+    }
+    var wasFound = false
+    var availableCodes = [];
+    iso6392Codes.forEach(function(codeRecord){
+        availableCodes.push(codeRecord.iso6392B);
+        if (codeRecord.iso6392B == language) {
+            wasFound = true;
+        }
+    });
+    if (!wasFound) {
+        throw new Error('Invalid language code ' + language + " Available: " + availableCodes.join(','))
+    }
+}
+
+
+function translate(object, language) {
+    if (typeof(object) !== 'object') {
+        throw new Error('Passed swagger schema must be object. Not ' + typeof(object))
+    }
+    object = Object.assign({}, object)
+    validateLanguageCode(language)
+    iterator(object, function (value, key, subject, path) {
+        if (typeof(key) === 'string') {
+            var matches = regexp.exec(key);
+            if (matches) {
+                var fieldName = matches[1];
+                if (typeof(subject[fieldName]) !== 'string') {
+                    throw new Error('Field ' + fieldName + ' at ' + path + ' must be filled and being string')
+                }
+                if (typeof(value) !== 'object') {
+                    throw new Error('Field ' + path + ' must be object')
+                }
+                if (typeof(value[language]) === 'undefined') {
+                    throw new Error('Field ' + path + ' does not contain language translation ' + language)
+                }
+                if (typeof(value[language]) !== 'string') {
+                    throw new Error('Field at ' + path + '.' + language + ' must be string ')
+                }
+                subject[fieldName] = value[language]
+                delete subject[key]
+            }
+        }
+    });
+    return object
+}
+
+function getUsedLanguageCodes(object) {
+    if (typeof(object) !== 'object') {
+        throw new Error('Passed swagger schema must be object. Not ' + typeof(object))
+    }
+    var result = [];
+    iterator(object, function (value, key, subject, path) {
+        if (typeof(key) === 'string') {
+            var matches = regexp.exec(key);
+            if (matches) {
+                if (typeof(value) !== 'object') {
+                    throw new Error('Field ' + path + ' must be object')
+                }
+                result = result.concat(Object.keys(value))
+                result = result.filter(function(value, index, self) {return self.indexOf(value) === index;})
+            }
+        }
+    })
+    result.forEach(function (languageCode) {
+        validateLanguageCode(languageCode)
+    })
+    return result
+}
+
+function validate(object, throwError) {
+    if (typeof(throwError) === 'undefined') {
+        throwError = false
+    }
+    try {
+        var languageCodes = getUsedLanguageCodes(object)
+        languageCodes.forEach(function(languageCode) {
+            translate(object, languageCode)
+        })
+    } catch (e) {
+        if (throwError) {
+            throw e
+        }
+        return false
+    }
+    return true
+}
+
+module.exports = {
+    translate: translate,
+    getUsedLanguageCodes: getUsedLanguageCodes,
+    validate: validate
+}
